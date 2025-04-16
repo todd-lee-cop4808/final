dfc0024e93914ec6f26a639cf91aee693afc268d
todd-lee-cop4808 <tlee2019@fau.edu>
2025-04-16 03:04:32 -0400

    remove keys

--- src/client.jsx
+++ src/client.jsx
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-const URL = "https://ytobukiyszoxfihxaoad.supabase.co";
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0b2J1a2l5c3pveGZpaHhhb2FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2OTQ0NzUsImV4cCI6MjA2MDI3MDQ3NX0.NBq1UR3i00ycTrMZeNUlwmDrO8SkpG326Sj9zdj4GTM";
+
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const API_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(URL, API_KEY);
